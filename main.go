53d7914bcac852d15241460b1c38a241e4a88e26
Dr Seokhyun Han <Seokhyun.Han@quant.network>
2021-02-07 18:34:42 +0000

    update

--- main.go
+++ main.go
@@ -1,23 +1,17 @@
 package main
 
 import (
-	"fmt"
+	//"github.com/golang/protobuf/proto"
 	"github.com/drhanlondon/protobuf-example-go/src/simple"
+	"fmt"
+	//"log"
+	
 )
 
 func main() {
-	doSimple()
-}
-
-func doSimple() {
 	sm := example_simple.SimpleMessage{
 		Id: 12345,
-		IsSimple: true,
-		Name: "Seokhyun",
-		SampleList: []int32{1,4,7,8},
+		
 	}
-
-	//fmt.Println(sm)
-	fmt.Println(sm.GetId())
-	fmt.Println(sm.Id)
 }
+
